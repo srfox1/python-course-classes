603312845b28ac43d519d68fd14e7489caed55e6
Sugarray Robinson <srfoxam@gmail.com>
2021-02-01 17:13:41 -0500

    added class inheritance

--- car.py
+++ car.py
@@ -24,11 +24,37 @@ class Car():
         else:
             print("You can't roll back the odometer!")
 
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+class ElectricCar(Car):
+    """Represen aspects of a car, specific to electric vehicles."""
+    def __init__(self, make, model, year):
+        """Initializes attributes of the parent class. Then Initializes atrributes specific to an electric car."""
+        super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+
+
+my_tesla = ElectricCar("tesla", "model s", 2016)
+print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
+
 
 my_new_car = Car('audi', 'a4', 2016)
-print(my_new_car.get_descriptive_name())
+print("\n" + my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
-my_new_car.update_odometer(23)
-my_new_car.read_odometer()
-my_new_car.update_odometer(11)
-my_new_car.read_odometer()
+
+my_used_car = Car("subaru", "outback", 2013)
+print("\n" + my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
